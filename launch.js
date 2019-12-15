dce6878bd8939bd3d071ecf5ead372e9cfd677a5
Joshua Ferris <jajjferris@gmail.com>
2019-12-15 11:41:57 -0500

    Updated pangination current page display and code tweaks

--- launch.js
+++ launch.js
@@ -1,56 +1,65 @@
 const logoElement = document.querySelector('#logo');
-const dateElement = document.querySelector('input');
+const dateYear = document.querySelector('#year');
+const dateMonth = document.querySelector('#month');
+const dateDay = document.querySelector('#day');
 const errorElement = document.querySelector('#error');
 const errorButtonElement = document.querySelector('#errorButton');
 const errorMessageElement = document.querySelector('#errorMessage');
 const birthdayLaunchesContainer = document.querySelector('#birthdayLaunches');
 const allBirthdayLaunchesContainer = document.querySelector('#allBirthdayLaunches');
-const allBirthdayLaunchesPagination = document.querySelector('#allBirthdayLaunchesPagination');
 const prevPaginationButton = document.querySelector('#prevPaginationButton');
 const nextPaginationButton = document.querySelector('#nextPaginationButton');
-
 const firstPaginationButton = document.querySelector('#firstPaginationButton');
 const lastPaginationButton = document.querySelector('#lastPaginationButton');
-
 const allLaunchesSince = document.querySelector('#allLaunchesSince');
 const paginationInfo = document.querySelector('#paginationInfo');
 
 const API_URL = 'https://launchlibrary.net/1.3/launch/';
-const FETCH_LIMIT = 20;
+const FETCH_LIMIT = 21;
+const errorMessages = {
+  noLaunchesFoundForDate: 'No launches found for that date.',
+  noLaunchesFoundLookBelow: 'No launches found for your birthday. Look below however for every launch since your birthday.',
+};
 
 let launchResponse = {};
 
 const getFormattedDate = (date) => {
   return new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
   .toISOString()
-  .split("T")[0];
+  .split('T')[0];
+};
+
+const navigateToElement = (element) => {
+  document.querySelector(element).scrollIntoView();
 };
 
 const makeRocketElement = ({ name, rocket: { imageURL }, vidURLs, infoURLs, failreason, net }) => {
   return `
-  <div class="column is-4">
-<div class="card" style="border-radius: 2rem;">
-  <div class="card-image" style="padding-top: 1rem;">
-    <img src="${imageURL}" style="max-height: 10rem;">
-  </div>
-  <div class="card-content">
-    <p class="title is-size-4 has-text-black" style="margin-bottom: 0;">${name}</p>
-    <small class="is-size-7 has-text-gray">${net}</small>
-    <p class="subtitle is-size-6 has-text-black">${failreason ? 'Launch Unsuccessful' : 'Launch Successful'}</p>
-    <small class="is-size-7 has-text-black">${failreason ? failreason : ''}</small>
-  </div>
-  <div class="card-footer">
-    <a href="${vidURLs && vidURLs.length > 0 ? vidURLs[0] : ''}" class="card-footer-item ${!vidURLs || vidURLs.length <= 0 ? ' has-text-grey' : ''}" target="_blank" rel="noopener noreferrer">Watch</a>
-    <a href="${infoURLs && infoURLs.length > 0 ? infoURLs[0] : ''}" class="card-footer-item ${!infoURLs || infoURLs.length <= 0 ? ' has-text-grey' : ''}" target="_blank" rel="noopener noreferrer">More info</a>
-  </div>
-</div></div>
-`;
+    <div class="column is-4">
+      <div class="card" style="border-radius: 2rem;">
+        <div class="card-image" style="padding-top: 1rem;">
+          <img src="${imageURL}" style="max-height: 10rem;">
+        </div>
+        <div class="card-content">
+          <p class="title is-size-4 has-text-black" style="margin-bottom: 0;">${name}</p>
+          <small class="is-size-7 has-text-gray">${net}</small>
+          <p class="subtitle is-size-6 has-text-black">${failreason ? 'Launch Unsuccessful' : 'Launch Successful'}</p>
+          <small class="is-size-7 has-text-black">${failreason ? failreason : ''}</small>
+        </div>
+        <div class="card-footer">
+          <a href="${vidURLs && vidURLs.length > 0 ? vidURLs[0] : ''}" class="card-footer-item ${!vidURLs || vidURLs.length <= 0 ? ' has-text-grey' : ''}" target="_blank" rel="noopener noreferrer">Watch</a>
+          <a href="${infoURLs && infoURLs.length > 0 ? infoURLs[0] : ''}" class="card-footer-item ${!infoURLs || infoURLs.length <= 0 ? ' has-text-grey' : ''}" target="_blank" rel="noopener noreferrer">More info</a>
+        </div>
+      </div>
+    </div>
+  `;
 };
 
 const makeRocketFetch = async (startDate, endDate, offset = 0) => {
   const response = await fetch(`${API_URL}${startDate}/${endDate}?limit=${FETCH_LIMIT}&offset=${offset}`);
   const data = await response.json();
-  if (data.launches.length <= 0) throw new Error('No launches found for that date.');
+  if (data.launches.length <= 0 || data.launches.length <= 0) throw new Error(errorMessages.noLaunchesFoundForDate);
+  if (data.status === 'error') throw new Error(data.msg);
   return {
     ...data,
     startDate,
@@ -59,8 +68,9 @@ const makeRocketFetch = async (startDate, endDate, offset = 0) => {
 };
 
 const updatePaginationButtons = () => {
-  const upperLimit = launchResponse.count < FETCH_LIMIT ? launchResponse.count : FETCH_LIMIT;
-  paginationInfo.textContent = `Showing: ${launchResponse.offset}-${launchResponse.offset + upperLimit}/${launchResponse.total}`;
+  const totalPages = Math.ceil(launchResponse.total / FETCH_LIMIT);
+  const pageLocation = `Page ${(launchResponse.offset/FETCH_LIMIT)+1} of ${totalPages}`;
+  paginationInfo.textContent = pageLocation;
 
   if (launchResponse.offset === 0) firstPaginationButton.setAttribute('disabled', true);
   else firstPaginationButton.removeAttribute('disabled');
@@ -102,6 +112,7 @@ const handlePaginationButton = async (direction) => {
         break;
     }
     launchResponse = { ...data };
+    navigateToElement('.navbar');
     updatePaginationButtons();
     updateDisplayedRockets();
   } catch (error) {
@@ -115,45 +126,61 @@ const updateDisplayedRockets = () => {
   launchResponse.launches.forEach((launch) => {
     rocketsString += makeRocketElement(launch);
   });
-  if (!allBirthdayLaunchesContainer.innerHTML) {
-    const title = document.createElement('h2');
-    title.classList.add('title', 'is-size-3');
-    title.textContent = 'All Rocket Launches Since Your Birthday!';
-    allLaunchesSince.prepend(title);
-  }
   allBirthdayLaunchesContainer.innerHTML = rocketsString;
 };
 
-const fetchRocketData = async () => {
+const checkDateValidity = () => {
+  if (!dateYear.validity.valid) throw new Error(dateYear.validationMessage);
+  if (!dateMonth.validity.valid) throw new Error(dateMonth.validationMessage);
+  if (!dateDay.validity.valid) throw new Error(dateDay.validationMessage);
+};
+
+const getEndDate = () => {
+  const curDate = new Date();
+  const year = dateYear.value;
+  const month = dateMonth.value;
+  const day = dateDay.value;
+  const endDate = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getFullYear() === Number(year) && curDate.getMonth() + 1 === Number(month) ? Number(day) + 1 : Number(day));
+  return endDate;
+};
+
+const resetResults = () => {
+  allLaunchesSince.style.display = 'none';
+  birthdayLaunchesContainer.innerHTML = '';
+  allBirthdayLaunchesContainer.innerHTML = '';
+};
+
+const addSearchResultsToPage = () => {
+  let nonMatches = 0;
+  const searchDate = `${dateYear.value}-${dateMonth.value}-${dateDay.value}`;
+  launchResponse.launches.forEach((launch) => {
+    if (getFormattedDate(new Date(launch.net)) !== searchDate) {
+      if (launchResponse.launches.length === 1) {
+        throw new Error(errorMessages.noLaunchesFoundLookBelow);
+      } else {
+        nonMatches += 1;
+        return;
+      }
+    } else {
+      birthdayLaunchesContainer.innerHTML += makeRocketElement(launch);
+    }
+  });
+  if (nonMatches === launchResponse.launches.length) throw new Error(errorMessages.noLaunchesFoundLookBelow);
+};
+
+const searchForRocketLaunches = async () => {
   try {
-    if (!dateElement.validity.valid) throw new Error(dateElement.validationMessage);
+    checkDateValidity();
+    resetResults();
     logoElement.classList.toggle('animate', true);
-    const curDate = new Date();
-    const [year, month, day] = dateElement.value.split('-');
-    const endDate = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getFullYear() === Number(year) && curDate.getMonth() + 1 === Number(month) ? Number(day) + 1 : Number(day));
-    const data = await makeRocketFetch(dateElement.value, getFormattedDate(endDate));
-    launchResponse = { ...data };
+    const searchDate = `${dateYear.value}-${dateMonth.value}-${dateDay.value}`;
+    const endDate = getEndDate();
+    launchResponse = await makeRocketFetch(searchDate, getFormattedDate(endDate));
     allLaunchesSince.style.display = '';
     updatePaginationButtons();
     updateDisplayedRockets();
-    if (data.status === 'error' && data.msg === 'None found') throw new Error('No launches found for your birthday. Look below however for every launch since your birthday.');
-    else if (data.status === 'error') throw new Error(data.msg);
-    if (data.launches.length <= 0) throw new Error('No launches found for your birthday. Look below however for every launch since your birthday.');
     errorElement.classList.toggle('hidden', true);
-    let nonMatches = 0;
-    data.launches.forEach((launch) => {
-      if (getFormattedDate(new Date(launch.net)) !== dateElement.value) {
-        if (data.launches.length === 1) {
-          throw new Error('No launches found for your birthday. Look below however for every launch since your birthday.');
-        } else {
-          nonMatches += 1;
-          return;
-        }
-      } else {
-        birthdayLaunchesContainer.innerHTML = makeRocketElement(launch);
-      }
-    });
-    if (nonMatches === data.launches.length) throw new Error('No launches found for your birthday. Look below however for every launch since your birthday.');
+    addSearchResultsToPage();
   } catch (error) {
     birthdayLaunchesContainer.innerHTML = '';
     errorElement.classList.toggle('hidden', false);
@@ -171,11 +198,23 @@ errorButtonElement.addEventListener('click', () => {
   errorElement.classList.toggle('hidden', true);
 });
 
-dateElement.addEventListener('keyup', (event) => {
+dateYear.addEventListener('keyup', (event) => {
   if (event.keyCode === 13) {
-    fetchRocketData();
+    searchForRocketLaunches();
   }
 });
 
-logoElement.addEventListener('click', fetchRocketData);
+dateMonth.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    searchForRocketLaunches();
+  }
+});
+
+dateDay.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    searchForRocketLaunches();
+  }
+});
+
+logoElement.addEventListener('click', searchForRocketLaunches);
 
